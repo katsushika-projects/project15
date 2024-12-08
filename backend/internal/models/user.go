1cc07dfde222539858922c99df8b249017908a00
nakajimamoto340 <nakajimamoto340@gmail.com>
2024-12-08 15:20:03 +0900

    idをuuidに修正

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,7 +1,20 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
+	ID       string `gorm:"type:text;primaryKey"`
 	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
+}
+
+// BeforeCreateフックでUUIDを生成
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if u.ID == "" {
+		u.ID = uuid.New().String() // UUIDを文字列として生成
+	}
+	return nil
 }
